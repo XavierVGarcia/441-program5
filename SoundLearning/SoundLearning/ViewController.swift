1bf93beac34f0b21cb0e6317ba65ded91b32d92b
Xavier Garcia <xaviergarcia@Xaviers-iMac.local>
2021-03-15 07:42:18 -0700

    started program 5

---
+++ SoundLearning/SoundLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundLearning
+//
+//  Created by Xavier Garcia on 3/15/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
