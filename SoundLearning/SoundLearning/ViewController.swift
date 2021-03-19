20d37a380f67e6e7bc690f1de1109f8e8bc0f12d
Xavier Garcia <xaviergarcia@Xaviers-iMac.local>
2021-03-19 08:35:49 -0700

    project 5 progress

--- SoundLearning/SoundLearning/ViewController.swift
+++ SoundLearning/SoundLearning/ViewController.swift
@@ -7,16 +7,37 @@
 
 import UIKit
 import AVFoundation
-let tString = "test string"
-var utterance = AVSpeechUtterance(string: tString)
-
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    func testSpeech(){ AVSpeechSynthesizer().speak(utterance) }
+    //could be improved by assigning a string to a location and then
+    //on click passing the string to the utterenace but Im not good enough for that yet
+    @IBAction func redButton(_ sender: UIButton) {
+        let utterance = AVSpeechUtterance(string: "Red!")
+        AVSpeechSynthesizer().speak(utterance)
+    }
+    @IBAction func orangeButton(_ sender: UIButton) {
+        let utterance = AVSpeechUtterance(string: "Orange!")
+        AVSpeechSynthesizer().speak(utterance)
+    }
+    @IBAction func yellowButton(_ sender: UIButton) {
+        let utterance = AVSpeechUtterance(string: "Yellow!")
+        AVSpeechSynthesizer().speak(utterance)
+    }
+    @IBAction func greenButton(_ sender: UIButton) {
+        let utterance = AVSpeechUtterance(string: "Green!")
+        AVSpeechSynthesizer().speak(utterance)
+    }
+    @IBAction func blueButton(_ sender: UIButton) {
+        let utterance = AVSpeechUtterance(string: "Blue!")
+        AVSpeechSynthesizer().speak(utterance)
+    }
+    @IBAction func purpleButton(_ sender: UIButton) {
+        let utterance = AVSpeechUtterance(string: "Purple!")
+        AVSpeechSynthesizer().speak(utterance)
+    }
 }
 
